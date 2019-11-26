ae6ddfe69c447d12e9ae88f37247fa5f05a4d82e
= <=>
2019-11-26 16:10:07 +0200

    update

--- work/urls.py
+++ work/urls.py
@@ -3,12 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:company_id>/', views.detail, name='detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
-
-# app_name = 'work'
-# urlpatterns = [
-#     path('', views.IndexView.as_view(), name='index'),
-#     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-# ]
--- work/views.py
+++ work/views.py
@@ -3,33 +3,16 @@ from django.http import Http404
 from .models import Company
 from django.views import generic
 
-def index(request):
-    companies = Company.objects.all()
-    context = {
-        'companies': companies,
-    }
-    return render(request, 'work/index.html', context)
+
+class IndexView(generic.ListView):
+    template_name = 'work/index.html'
+    context_object_name = 'companies'
+
+    def get_queryset(self):
+        return Company.objects.all()
 
 
-def detail(request, company_id):
-    try:
-        company = Company.objects.get(pk=company_id)
-    except Company.DoesNotExist:
-    	  raise Http404("Company does not exist")
-    return render(request, 'work/detail.html', {'company': company})
-
-
-
-
-# class IndexView(generic.ListView):
-#     template_name = 'work/index.html'
-#     context_object_name = 'companies'
-
-#     def get_queryset(self):
-#         return Company.objects.all()
-
-
-# class DetailView(generic.DetailView):
-#     model = Company
-#     template_name = 'work/detail.html'
+class DetailView(generic.DetailView):
+    model = Company
+    template_name = 'work/detail.html'
 
