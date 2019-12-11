b32b9ae79e9200cdfcc6c1f69fed618126ba8791
= <=>
2019-12-11 13:19:19 +0200

    flake8

--- work/views.py
+++ work/views.py
@@ -4,14 +4,12 @@ from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.utils import timezone
-from django.http import Http404
-from django.shortcuts import get_object_or_404
 from .models import (
     Company, Work, Worker, WorkTime, WorkPlace)
 from .forms import (
         CreateWorkTime, )
 from django.views.generic import (
-    View, ListView, DetailView,CreateView, FormView)
+    View, ListView, DetailView, CreateView, FormView)
 from django.views.generic.detail import SingleObjectMixin
 import logging
 
@@ -77,11 +75,11 @@ class WorkerWT(SingleObjectMixin, FormView):
             wt.worker = current_worker
             wt.save()
             return redirect('work:worker_detail', kwargs['pk'])
-        
+
         logger.info('Form is invalid')
-    
+
         # return ???
-        
+
 
 class WorkerDetail(View):
 
@@ -113,10 +111,12 @@ class Hire(CreateView):
         current_worker = form.cleaned_data['worker']
 
         logger.info(
-            f'Current worker: {current_worker.first_name} {current_worker.last_name}')
+            f'Current worker:'
+            f'{current_worker.first_name} {current_worker.last_name}')
 
         if WorkPlace.objects.filter(worker=current_worker).exists():
-            prev_wp = WorkPlace.objects.filter(worker=current_worker).latest('id')
+            prev_wp = WorkPlace.objects.filter(
+                worker=current_worker).latest('id')
 
             logger.info(
                 f'Change status to Finished for: {prev_wp}')
