4648b1b66cf73eb34a389e8e0b753f2882dcb59c
= <=>
2019-12-11 13:19:46 +0200

    flake8

--- work/forms.py
+++ work/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from .models import WorkTime, WorkPlace
 
+
 class CreateWorkTime(forms.ModelForm):
     # def __init__(self, w_id=None, *args, **kwargs):
     #     super().__init__(*args, **kwargs)
-    #     self.fields['workplace'].queryset = forms.ModelChoiceField(queryset=WorkPlace.objects.filter(worker_id=1))
+    #     self.fields['workplace'].queryset = forms.ModelChoiceField(
+    #                 queryset=WorkPlace.objects.filter(worker_id=1))
 
     class Meta:
         model = WorkTime
