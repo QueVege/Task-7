47de8ca341605f05fb64387f74e8113dd3feff10
= <=>
2019-12-17 16:13:13 +0200

    add authentication

--- work_project/urls.py
+++ work_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('work.urls')),
+    path('', include('work.urls', namespace='work')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
