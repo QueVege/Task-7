2ec4021c62d0a17728e543466ce99905f28d8ff2
Valeria Berdnikova <likespony@gmail.com>
2019-12-20 00:52:36 +0200

    update

--- work/tasks.py
+++ work/tasks.py
@@ -1,5 +1,27 @@
-from celery import shared_task
+from work_project.celery import app
+from work.models import Worker
+import requests
+import json
 
-@shared_task
-def adding_task(x, y):
-    return x + y
+@app.task(name='work.tasks.create_workers')
+def create_workers():
+    connect_timeout, read_timeout = 5.0, 30.0
+    response = requests.get(
+        'https://jsonplaceholder.typicode.com/users',
+        timeout=(connect_timeout, read_timeout)
+    )
+    users_data = response.json()
+
+    for user in users_data:
+        name = user['name'].split(' ')
+        first_n = name[0]
+        last_n = ' '.join(name[1::]
+
+        if not Worker.objects.filter(
+            first_name=first_n).filter(last_name=last_n).exists():
+    
+            Worker.objects.create(
+                first_name=first_n,
+                last_name=last_n
+            )
+    
